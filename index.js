827263c3cbfa693c8d2f107160f1574c0d437408
Esther Rosales <estherrosales.erb@gmail.com>
2021-02-20 21:21:24 +0000

    generador de excusas

---
+++ index.js
@@ -0,0 +1,9 @@
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+let randomwho =who[Math.floor(Math.random() * who length)];
+let randomwhat =what[Math.floor(Math.random() * when length)];
+let randomwhen =when[Math.floor(Math.random() * when length)];
+console.log(random + " " + random + " " + random);
+document.querySelector("#fase").insertAdjacentText.html=resultado;
+
